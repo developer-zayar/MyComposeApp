04485b2b7e08a5be2cc30adf48374db544779d27
zyp.tijarah <zyp.tijarah@gmail.com>
2025-06-18 18:30:16 +0800

    create Woof app
    This commit introduces the Woof app, a simple application that displays a list of dogs with their names and ages.
    
    Key changes include:
    
    - Added a new WoofActivity and its layout.
    - Created a Dog data class.
    - Added dog data to Datasource.
    - Implemented DogItem composable to display individual dog information.
    - Added WoofTopAppBar with a logo and app name.
    - Defined custom typography and shapes for the Woof app.
    - Updated color schemes and themes.
    - Added necessary string resources and drawable assets.
    - Set WoofActivity as the launcher activity in the manifest.

---
+++ app/src/main/java/com/zayar/mycomposeapp/models/Dog.kt
@@ -0,0 +1,11 @@
+package com.zayar.mycomposeapp.models
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Dog(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val name: Int,
+    val age: Int,
+    @StringRes val hobbies: Int
+)
